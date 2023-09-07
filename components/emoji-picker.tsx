80471e47c748a6378b424dcd4e92bbad05e8a771
FelicixAwe <58763387+FelicixAwe@users.noreply.github.com>
2023-09-07 09:06:47 -0700

    debug emoji bar problem

--- components/emoji-picker.tsx
+++ components/emoji-picker.tsx
@@ -4,6 +4,7 @@ import { Smile } from "lucide-react";
 import Picker from "@emoji-mart/react";
 import data from "@emoji-mart/data";
 import { useTheme } from "next-themes";
+import dynamic from "next/dynamic";
 
 import {
   Popover,
@@ -15,20 +16,17 @@ interface EmojiPickerProps {
   onChange: (value: string) => void;
 }
 
-export const EmojiPicker = ({
-  onChange,
-}: EmojiPickerProps) => {
+export const EmojiPicker = ({ onChange }: EmojiPickerProps) => {
+  const Picker = dynamic(() => import("@emoji-mart/react"), { ssr: false });
   const { resolvedTheme } = useTheme();
 
   return (
     <Popover>
       <PopoverTrigger>
-        <Smile
-          className="text-zinc-500 dark:text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-300 transition"
-        />
+        <Smile className="text-zinc-500 dark:text-zinc-400 hover:text-zinc-600 dark:hover:text-zinc-300 transition" />
       </PopoverTrigger>
-      <PopoverContent 
-        side="right" 
+      <PopoverContent
+        side="right"
         sideOffset={40}
         className="bg-transparent border-none shadow-none drop-shadow-none mb-16"
       >
@@ -39,5 +37,5 @@ export const EmojiPicker = ({
         />
       </PopoverContent>
     </Popover>
-  )
-}
+  );
+};
